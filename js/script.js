e5426012e1f05157f8c4f751b676157082fedc65
RaimondoRinaldi85 <bar@example.com>
2020-09-22 18:54:49 +0200

    22092020-18.54

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@ document.querySelector(".scroll-btn").addEventListener("click", () => {
         document.querySelector("html").style.scrollBehavior = "smooth";
     }, 1000);
 });
+
+
+
+
+
+const burger = document.getElementById('burger');
+const ul = document.querySelector('nav ul');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('show-x');
+    ul.classList.toggle('show');
+});
