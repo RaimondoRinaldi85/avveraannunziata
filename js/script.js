13056cb3c5f0f8c36778e1522d7b17a42096dde8
RaimondoRinaldi85 <bar@example.com>
2020-10-04 17:38:45 +0200

    04102020-17.38

--- js/script.js
+++ js/script.js
@@ -61,3 +61,46 @@ function closeModal(modal) {
     modal.classList.remove('active')
     overlay.classList.remove('active')
 }
+
+
+
+
+
+
+
+/* modal2 */
+
+
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal2 = document.querySelector(button.dataset.modalTarget)
+        openModal(modal2)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal2.active')
+    modals.forEach(modal2 => {
+        closeModal(modal2)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal2 = button.closest('.modal2')
+        closeModal(modal2)
+    })
+})
+
+function openModal(modal2) {
+    if (modal2 == null) return
+    modal2.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal2) {
+    if (modal2 == null) return
+    modal2.classList.remove('active')
+    overlay.classList.remove('active')
+}
